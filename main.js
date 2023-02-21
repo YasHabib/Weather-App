81817f626007e5bf2b1cbf9d16edef33722f3bda
YasHabib <yasin_habib@outlook.com>
2023-02-21 12:04:27 -0700

    Integrated Open Meteo API

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+import { getWeather } from "./public/weather"
 import "./style.css"
+
+
+getWeather(10,10, Intl.DateTimeFormat().resolvedOptions().timeZone).then(
+    res => {
+
+        console.log(res.data)
+    })
